d679c75626120adc82a7b65b5b5d2dfd88b74379
Jari Aalto <jari.aalto@cante.net>
2012-10-09 10:03:13 +0300

    import original  CYGWIN-PATCHES 2012-10-09

---
+++ test/test.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Test basic functionality
+
+set -e
+
+proram=$0
+TMPDIR=${TMPDIR:-/tmp}
+BASE=tmp.$$
+TMPBASE=${TMPDIR%/}/$BASE
+CURDIR=.
+
+case "$0" in
+  */*)
+        CURDIR=$(cd "${0%/*}" && pwd)
+        ;;
+esac
+
+AtExit ()
+{
+    rm -f "$TMPBASE"*
+}
+
+Run ()
+{
+    echo "$*"
+    shift
+    eval "$@"
+}
+
+trap AtExit 0 1 2 3 15
+
+# #######################################################################
+
+file="$CURDIR/example.dat"
+
+Run "%% TEST <case>:" echo $file
+
+# End of file
